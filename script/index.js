0c0fcbb7abd63f4b7eea6a39dbf7e25e64845438
Fegie.c <fegie.c@LGroup.global>
2023-02-14 18:33:20 +0800

    Signed-off-by

--- script/index.js
+++ script/index.js
@@ -1,3 +1,7 @@
 function buttons_click_1() {
     window.open("https://www.w3schools.com/jsref/event_onclick.asp")
 };
+
+function buttons_click_2() {
+    window.open("https://www.w3schools.com/jsref/event_onclick.asp", true)
+};
