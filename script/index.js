8761902aad2660fe77f24d5d5dbf0fbee4343ad7
Fegie.c <fegie.c@LGroup.global>
2023-02-15 11:02:41 +0800

    Signed-off-by: Fegie.c <fegie.c@LGroup.global>

--- script/index.js
+++ script/index.js
@@ -1,19 +1,19 @@
 // 禁用雙指放大
-// document.documentElement.addEventListener('touchstart', function (event) {
-//     if (event.touches.length > 1) {
-//         event.preventDefault();
-//     }
-// }, false);
+document.documentElement.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, false);
 
-// // 禁用雙擊放大
-// var lastTouchEnd = 0;
-// document.documentElement.addEventListener('touchend', function (event) {
-//     var now = Date.now();
-//     if (now - lastTouchEnd <= 300) {
-//         event.preventDefault();
-//     }
-//     lastTouchEnd = now;
-// }, false);
+// 禁用雙擊放大
+var lastTouchEnd = 0;
+document.documentElement.addEventListener('touchend', function (event) {
+    var now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
 
 // 依tg主題變色
 function setThemeClass() {
@@ -23,6 +23,7 @@ Telegram.WebApp.onEvent('themeChanged', setThemeClass);
 setThemeClass();
 
 
+// 按鈕連結區
 function buttons_click_1() {
     window.open("https://www.w3schools.com/jsref/event_onclick.asp")
 };
