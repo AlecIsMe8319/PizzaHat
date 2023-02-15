de655449d947b6f10d01542199262e098bc9e894
Fegie.c <fegie.c@LGroup.global>
2023-02-15 10:26:51 +0800

    Signed-off-by: Fegie.c <fegie.c@LGroup.global>

--- script/index.js
+++ script/index.js
@@ -1,3 +1,21 @@
+// 禁用雙指放大
+document.documentElement.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, false);
+
+// 禁用雙擊放大
+var lastTouchEnd = 0;
+document.documentElement.addEventListener('touchend', function (event) {
+    var now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
+
+
 function buttons_click_1() {
     window.open("https://www.w3schools.com/jsref/event_onclick.asp")
 };
