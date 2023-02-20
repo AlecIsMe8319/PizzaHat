d9cf679db352e329ded8ac841f223c3b8aab733f
Fegie.c <fegie.c@LGroup.global>
2023-02-20 11:35:44 +0800

    Signed-off-by: Fegie.c <fegie.c@LGroup.global>

--- script/overtime.js
+++ script/overtime.js
@@ -25,8 +25,42 @@ function setDate() {
     var d = new Date().toLocaleDateString();
     document.getElementById("applydate").value = d;
 };
+
+// 新增明細表
 var clicktime = 0;
 function plusbutton() {
     clicktime = clicktime + 1;
-    console.log(clicktime);
+    // console.log(clicktime);
+    var detail = document.querySelector(".detail");
+    var newdiv = document.createElement("cm");
+    newdiv.innerHTML = `<div class="details ${clicktime}">\
+    <div class="detail-rowno- ${clicktime}">\
+        序号\
+    </div>\
+    <div class="detail-reason- ${clicktime}">\
+        加班原因\
+    </div>\
+    <div class="detail-location- ${clicktime}">\
+        加班地点\
+    </div>\
+    <div class="detail-overtime-date- ${clicktime}">\
+        加班日期\
+    </div>\
+    <div class="detail-overtime-start- ${clicktime}">\
+        加班开始时间\
+    </div>\
+    <div class="detail-overtime-end- ${clicktime}">\
+        加班结束时间\
+    </div>\
+    <div class="detail-overtime-break- ${clicktime}">\
+        加班休息小时\
+    </div>\
+    <div class="detail-overtime-time- ${clicktime}">\
+        加班时长\
+    </div>\
+    <div class="detail-settle-up- ${clicktime}">\
+        结算方式\
+    </div>\
+</div>`;
+    detail.appendChild(newdiv);
 }
